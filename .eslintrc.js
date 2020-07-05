b0d2f08125b32d28381f514409df27cc8faf7cac
Dohyung Ahn <me@rinae.dev>
2020-07-05 13:08:57 +0900

    chore: add `simple-import-sort` eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
@@ -23,5 +26,7 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'react/prop-types': 0,
     '@typescript-eslint/no-empty-interface': 0,
+    'simple-import-sort/sort': 'warn',
+    'prettier/prettier': 'warn',
   },
 };
